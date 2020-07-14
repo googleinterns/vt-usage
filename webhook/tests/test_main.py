5c19db35279da07f4b0668b333797a2a595bf3d9
Michał Niedziółka <michalekniedziolka@gmail.com>
2020-07-14 12:39:34 +0000

    Add requirements.txt and tests for main.py

---
+++ webhook/tests/test_main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+def test_index():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.text == '{"data":"Hello World"}'
