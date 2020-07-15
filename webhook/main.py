507f5bc69dff3dc806e95a7d88ad1e2f10722cbc
Michał Niedziółka <michalekniedziolka@gmail.com>
2020-07-15 23:08:23 +0000

    Add new structure in query-results

--- webhook/main.py
+++ webhook/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Request, Header, HTTPException
+from fastapi import FastAPI, Request, Header, HTTPException, Body
+from pydantic import BaseModel
+from typing import List, Optional, Dict, Union
 
 app = FastAPI()
 
@@ -8,17 +10,14 @@ async def root():
     return {"data": "Hello World"}
 
 
+class VTAPI(BaseModel):
+  data: Union[str, List, Dict]
+  links: Optional[Dict]
+  meta: Optional[Dict]
+
 @app.post("/query-results/")
-async def query_results(request: Request):
-    if request.headers.get("X-Appengine-Inbound-Appid", None) != 'virustotal-step-2020':
+async def query_results(data: VTAPI, x_appengine_inbound_appid: Optional[str] = Header(None)):
+    if x_appengine_inbound_appid != 'virustotal-step-2020':
         raise HTTPException(status_code=403, detail="Access forbidden")
 
-    try:
-        data = await request.json()
-    except RuntimeError:
-        data = None
-
-    if "data" not in data:
-        raise HTTPException(status_code=400, detail="Bad request")
-
-    print(data)
+    return data
--- webhook/tests/test_main.py
+++ webhook/tests/test_main.py
@@ -5,7 +5,76 @@ from webhook.main import app
 
 client = TestClient(app)
 
+
 def test_index():
-    response = client.get('/')
-    assert response.status_code == 200
-    assert response.json() == {"data":"Hello World"}
+    r = client.get('/')
+    assert r.status_code == 200
+    assert r.json() == {"data": "Hello World"}
+
+
+correct_headers = {"X-Appengine-Inbound-Appid": "virustotal-step-2020"}
+
+
+def test_query_results_str():
+    r = client.post(
+        '/query-results/',
+        headers=correct_headers,
+        json={"data": "str"})
+    assert r.status_code == 200
+    assert "data" in r.json()
+    assert r.json()["data"] == "str"
+
+
+def test_query_results_list():
+    r = client.post(
+        '/query-results/',
+        headers=correct_headers,
+        json={"data": []})
+    assert r.status_code == 200
+    assert "data" in r.json()
+    assert r.json()["data"] == []
+
+
+def test_query_results_dict():
+    r = client.post(
+        '/query-results/',
+        headers=correct_headers,
+        json={"data": {}})
+    assert r.status_code == 200
+    assert r.json()["data"] == {}
+
+
+def test_query_results_wrong_headers():
+    r = client.post(
+        '/query-results/',
+        headers={"Some": "nonsense"},
+        json={"data": {}})
+
+    assert r.status_code == 403
+    assert r.json() == {"detail": "Access forbidden"}
+
+
+def test_query_results_no_data():
+    r = client.post(
+        '/query-results/',
+        headers=correct_headers,
+        json={"links": {"self": "https://www.virustotal.com"}})
+
+    assert r.status_code == 422
+    assert r.json()["detail"][0]["type"] == "value_error.missing"
+
+
+def test_query_results_additional_fields():
+    r = client.post(
+        '/query-results/',
+        headers=correct_headers,
+        json={
+            "data": [{}, {"another": "dict"}, {}],
+            "links": {"self": "https://www.virustotal.com"},
+            'meta': {'count': 0}
+        })
+
+    assert r.status_code == 200
+    assert r.json()["data"] == [{}, {"another": "dict"}, {}]
+    assert r.json()["links"] == {"self": "https://www.virustotal.com"}
+    assert r.json()["meta"] == {'count': 0}
