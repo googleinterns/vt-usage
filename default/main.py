a6b1c02820e629a8c288e0d6215a37deb7ee3a84
Egor Karpov <karpove@google.com>
2020-07-15 18:27:37 +0300

    Endpoint for user configuration (#6)

--- default/main.py
+++ default/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from google.cloud import datastore
 
 app = FastAPI()
 
@@ -8,7 +9,20 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 
 templates = Jinja2Templates(directory='templates')
 
+datastore_client = datastore.Client()
+
 
 @app.get('/')
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {'request': request, 'content': 'Hello, world! =)'})
+    return templates.TemplateResponse("form.html", {'request': request, 'content': 'Hello, world! =)'})
+
+
+@app.post('/userdata/')
+async def userdata(request: Request, apikey: str = Form(...), webhook: str = Form(...)):
+    task = datastore.Entity(datastore_client.key('userdata'))
+    task.update({
+        'apikey': apikey,
+        'webhook': webhook,
+    })
+    datastore_client.put(task)
+    return templates.TemplateResponse("index.html", {'request': request, 'content': 'Your answer successfully saved'})
--- default/tests/test_main.py
+++ default/tests/test_main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-from default.main import app
+from default.main import app, datastore_client
 
 client = TestClient(app)
 
@@ -10,3 +10,22 @@ def test_index():
     assert response.status_code == 200
     with open('tests/testfiles/index.html', 'r') as correct:
         assert response.text == correct.read()
+
+
+def test_userdata():
+    response = client.post(
+        '/userdata/',
+        data={
+            'apikey': 'test_apikey',
+            'webhook': 'test_webhook',
+        })
+    assert response.status_code == 200
+    with open('tests/testfiles/userdata.html', 'r') as correct:
+        assert response.text == correct.read()
+    
+    query = datastore_client.query(kind='userdata')
+    query.add_filter('apikey', '=', 'test_apikey')
+    result = list(query.fetch())
+    assert result != []
+    key = result[0].key
+    datastore_client.delete(key)
