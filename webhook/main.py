2206e17262b59112777ade280756f02096397d29
Michał Niedziółka <michalekniedziolka@gmail.com>
2020-07-15 14:26:24 +0000

    Add query_results in main.py

--- webhook/main.py
+++ webhook/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Header, HTTPException
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"data": "Hello World"}
+
+
+@app.post("/query-results/")
+async def query_results(request: Request):
+    if request.headers.get("X-Appengine-Inbound-Appid", None) != 'virustotal-step-2020':
+        raise HTTPException(status_code=403, detail="Access forbidden")
+
+    try:
+        data = await request.json()
+    except RuntimeError:
+        data = None
+
+    if "data" not in data:
+        raise HTTPException(status_code=400, detail="Bad request")
+
+    print(data)
