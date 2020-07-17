19cd44ff7284bf05e9055ad17b376a0cee7785b5
niedziol <michalekniedziolka@gmail.com>
2020-07-17 10:37:08 +0000

    Add query_results in main.py (#13)

--- webhook/main.py
+++ webhook/main.py
@@ -1,7 +1,39 @@
-from fastapi import FastAPI, Request
+from enum import Enum
+from fastapi import FastAPI, Request, Header, HTTPException, Body
+from pydantic import BaseModel
+from typing import List, Optional, Dict, Union
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"data": "Hello World"}
+
+
+class VTType(str, Enum):
+    file = 'file'
+    url = 'url'
+    domain = 'domain'
+    ip_address = 'ip_address'
+
+
+class VTData(BaseModel):
+    attributes: Dict
+    id: str
+    links: Dict
+    type: VTType
+
+
+class VTAPI(BaseModel):
+    data: List[VTData]
+    links: Dict
+    meta: Dict
+
+
+@app.post("/query-results/")
+async def query_results(data: VTAPI, x_appengine_inbound_appid: Optional[str] = Header(None)):
+    if x_appengine_inbound_appid != 'virustotal-step-2020':
+        raise HTTPException(status_code=403, detail="Access forbidden")
+
+    return data
--- webhook/tests/test_main.py
+++ webhook/tests/test_main.py
@@ -5,7 +5,138 @@ from webhook.main import app
 
 client = TestClient(app)
 
+
 def test_index():
-    response = client.get('/')
-    assert response.status_code == 200
-    assert response.json() == {"data":"Hello World"}
+    r = client.get('/')
+    assert r.status_code == 200
+    assert r.json() == {"data": "Hello World"}
+
+
+correct_headers = {"X-Appengine-Inbound-Appid": "virustotal-step-2020"}
+
+
+def test_query_results_empty_list():
+    r = client.post(
+        '/query-results/',
+        headers=correct_headers,
+        json={"data": [], "links": {}, "meta": {}})
+
+    assert r.status_code == 200
+    result = r.json()
+    for name in ["data", "links", "meta"]:
+        assert name in result
+        assert len(result[name]) == 0
+
+
+def test_query_results_normal_request():
+    type_list = ["file", "url", "domain", "ip_address"]
+    r = client.post(
+        '/query-results/',
+        headers=correct_headers,
+        json={
+            "data": [{
+                "attributes": {},
+                "id": "",
+                "links": {},
+                "type": t
+            } for t in type_list],
+            "links": {},
+            "meta": {}
+        })
+
+    assert r.status_code == 200
+    result = r.json()
+    for obj in result["data"]:
+        assert obj["attributes"] == {}
+        assert obj["id"] == ""
+        assert obj["links"] == {}
+        assert obj["type"] in type_list
+
+
+def test_query_results_bad_type():
+    r = client.post(
+        '/query-results/',
+        headers=correct_headers,
+        json={
+            "data": [{
+                "attributes": {},
+                "id": "",
+                "links": {},
+                "type": "other_type"
+            }],
+            "links": {},
+            "meta": {}
+        })
+
+    assert r.status_code == 422
+
+
+def test_query_results_links_and_meta():
+    links = {
+        "self": "https://google.com",
+        "next": "https://gmail.com",
+        "strange": "not-url-for-sure"
+    }
+    meta = {
+        "count": 0,
+        "something": "other"
+    }
+
+    r = client.post(
+        '/query-results/',
+        headers=correct_headers,
+        json={"data": [], "links": links, "meta": meta})
+
+    assert r.status_code == 200
+    result = r.json()
+    assert result["data"] == []
+    assert result["links"] == links
+    assert result["meta"] == meta
+
+
+def test_query_results_wrong_headers():
+    r = client.post(
+        '/query-results/',
+        headers={},
+        json={"data": [], "links": {}, "meta": {}})
+
+    assert r.status_code == 403
+    assert r.json() == {"detail": "Access forbidden"}
+
+
+def test_query_missing_mandatory_field():
+    request = {
+        "data": [],
+        "links": {},
+        "meta": {}
+    }
+
+    for key in request.keys():
+        r = client.post(
+            '/query-results/',
+            headers=correct_headers,
+            json=request.copy().pop(key))
+
+        assert r.status_code == 422
+        assert r.json()["detail"][0]["type"] == "value_error.missing"
+
+
+def test_query_results_missing_field_in_data():
+    obj = {
+        "attributes": {},
+        "id": "",
+        "links": {},
+        "type": "file"
+    }
+
+    for key in obj.keys():
+        r = client.post(
+            '/query-results/',
+            headers=correct_headers,
+            json={
+                "data": [obj.copy().pop(key)],
+                "links": {},
+                "meta": {}
+            })
+
+        assert r.status_code == 422
