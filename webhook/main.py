8dc3524e2b34b64f618d93a99a9d62ad5a2f60b5
Michał Niedziółka <michalekniedziolka@gmail.com>
2020-07-17 16:20:51 +0000

    Add model for email with validators

--- webhook/main.py
+++ webhook/main.py
@@ -1,9 +1,12 @@
 from enum import Enum
 from fastapi import FastAPI, Request, Header, HTTPException, Body
-from pydantic import BaseModel
+from google.cloud import ndb
+from pydantic import BaseModel, validator
 from typing import List, Optional, Dict, Union
+from validate_email import validate_email
 
 app = FastAPI()
+client = ndb.Client()
 
 
 @app.get("/")
@@ -36,4 +39,29 @@ async def query_results(data: VTAPI, x_appengine_inbound_appid: Optional[str] = 
     if x_appengine_inbound_appid != 'virustotal-step-2020':
         raise HTTPException(status_code=403, detail="Access forbidden")
 
+    return data
+
+
+class Email(BaseModel):
+    api_key: str
+    email: str
+
+    @validator('api_key')
+    def api_key_validator(cls, v):
+        if not v.isalnum():
+            raise ValueError("API key must be alphanumeric!")
+        return v
+
+    @validator('email')
+    def email_validator(cls, v):
+        if not validate_email(email_address=v,
+                              check_regex=True,
+                              check_mx=True,
+                              debug=True):
+            raise ValueError("Email address is not valid!")
+        return v
+
+
+@app.put("/email-address/")
+async def set_email(data: Email):
     return data
