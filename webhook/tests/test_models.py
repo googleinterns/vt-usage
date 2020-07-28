6232fd51cb96afddcad31ba82c00fd20bfaa18e8
Michał Niedziółka <michalekniedziolka@gmail.com>
2020-07-28 14:18:50 +0000

    Add tests for correct email and api_key, fix ommiting exception

--- webhook/tests/test_models.py
+++ webhook/tests/test_models.py
@@ -3,20 +3,32 @@ from models import *
 import pytest
 
 
+@pytest.mark.parametrize("api_key", ["123456789", "abcdefg", "abc123"])
+def test_correct_api_key(api_key):
+    assert APIKey(api_key=api_key).api_key == api_key
+
+
 @pytest.mark.parametrize("api_key", ["\x00", "abc/abc", "abc'abc", "abc--abc"])
-def test_email_wrapper_bad_api_key(api_key):
+def test_bad_api_key(api_key):
     try:
         APIKey(api_key=api_key)
     except ValueError:
-        pass
- 
+        return
+
+    assert False
+
+
+@pytest.mark.parametrize("email", ["good@email.com", "strange@domain.sufix", "with.dot@address.com"])
+def test_api_key_email(email):
+    assert APIKeyEmail(api_key="abc123", email=email).email == email
 
 
 @pytest.mark.parametrize("email", ["without.domain", "mail@withoutsufix", "mail@withoutsufix",
                                    "without.at.com", "strange@\x0char.com"])
-def test_email_wrapper_bad_email(email):
+def test_api_key_email_bad_email(email):
     try:
         APIKeyEmail(api_key="abc123", email=email)
     except ValueError:
-        pass
+        return
 
+    assert False
