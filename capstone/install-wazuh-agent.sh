8752f1b9575af4e8897b43fd976c8497e308aa98
Michał Niedziółka <michalekniedziolka@gmail.com>
2020-07-29 14:26:12 +0000

    Add Wazuh agent install

---
+++ capstone/install-wazuh-agent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Fail if any command fails.
+set -e
+
+# Install needed packages.
+apt-get install curl apt-transport-https lsb-release gnupg2
+
+# Install the Wazuh repository GPG key.
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
+
+# Add the repository.
+echo "deb https://packages.wazuh.com/3.x/apt/ stable main" | tee /etc/apt/sources.list.d/wazuh.list
+
+# Update the package information.
+apt-get update
+
+# Install agent.
+WAZUH_MANAGER="wazuh-manager-instance" apt-get install wazuh-agent
+
+# Disable updates.
+sed -i "s/^deb/#deb/" /etc/apt/sources.list.d/wazuh.list
+apt-get update
