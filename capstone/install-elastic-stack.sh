e9e9bdb288999186de61def4f3560f1680344ea0
Michał Niedziółka <michalekniedziolka@gmail.com>
2020-08-03 16:48:09 +0000

    Add regexp to sed in elastic install

--- capstone/install-elastic-stack.sh
+++ capstone/install-elastic-stack.sh
@@ -13,17 +13,17 @@ apt-get -y update
 apt-get -y install elasticsearch=7.8.1
 
 # Bind Elastic to IP.
-sed "s/#network.host:*/network.host: \"elastic-stack-instance\"/g" /etc/elasticsearch/elasticsearch.yml
+sed -e "s/#network.host:*/network.host: \"elastic-stack-instance\"/g" /etc/elasticsearch/elasticsearch.yml
 
 # Create or uncomment node.
 if grep -Fxq "#node.name:" /etc/elasticsearch/elasticsearch.yml; then
-    sed "s/#node.name:*/node.name: \"Capstone\"/g" /etc/elasticsearch/elasticsearch.yml
+    sed -e "s/#node.name:*/node.name: \"Capstone\"/g" /etc/elasticsearch/elasticsearch.yml
 else
     echo "node.name: \"Capstone\"" >> /etc/elasticsearch/elasticsearch.yml
 fi
 
 if grep -Fxq "#cluster.initial_master_nodes" /etc/elasticsearch/elasticsearch.yml; then
-    sed "s/#cluster.initial_master_nodes:*/cluster.initial_master_nodes: [\"Capstone\"]/g" /etc/elasticsearch/elasticsearch.yml
+    sed -e "s/#cluster.initial_master_nodes:*/cluster.initial_master_nodes: [\"Capstone\"]/g" /etc/elasticsearch/elasticsearch.yml
 else
     echo "cluster.initial_master_nodes: [\"Capstone\"]" >> /etc/elasticsearch/elasticsearch.yml
 fi
