c652e900996c3c736bb938a1bb63f2cf49ae8cb3
Michał Niedziółka <michalekniedziolka@gmail.com>
2020-08-04 19:10:56 +0000

    Remove sed and if statements

--- capstone/install-elastic-stack.sh
+++ capstone/install-elastic-stack.sh
@@ -12,22 +12,11 @@ apt-get -y update
 # Install Elasticsearch.
 apt-get -y install elasticsearch=7.8.1
 
-# Kropka jest znakiem specjalnym.
 # Bind Elastic to IP.
-sed "s/#network.host:.*/network.host: \"elastic-stack-instance\"/" /etc/elasticsearch/elasticsearch.yml
+echo "network.host: \"elastic-stack-instance\"" >> /etc/elasticsearch/elasticsearch.yml
 
-# Create or uncomment node.
-if [ grep -xq "#node.name:" /etc/elasticsearch/elasticsearch.yml -eq 0 ]; then
-    sed -e "s/#node.name:.*/node.name: \"Capstone\"/" /etc/elasticsearch/elasticsearch.yml
-else
-    echo "node.name: \"Capstone\"" >> /etc/elasticsearch/elasticsearch.yml
-fi
-
-if [ grep -xq "#cluster.initial_master_nodes" /etc/elasticsearch/elasticsearch.yml -eq 0 ]; then
-    sed "s/#cluster.initial_master_nodes:*/cluster.initial_master_nodes: [\"Capstone\"]/" /etc/elasticsearch/elasticsearch.yml
-else
-    echo "cluster.initial_master_nodes: [\"Capstone\"]" >> /etc/elasticsearch/elasticsearch.yml
-fi
+echo "node.name: \"Capstone\"" >> /etc/elasticsearch/elasticsearch.yml
+echo "cluster.initial_master_nodes: [\"Capstone\"]" >> /etc/elasticsearch/elasticsearch.yml
 
 # Start Elasticsearch service
 systemctl daemon-reload
