280f0c298eba584c4c9ecc88f25a67e8a0b7bc0c
Michał Niedziółka <michalekniedziolka@gmail.com>
2020-08-04 19:06:34 +0000

    Fix grep command

--- capstone/install-elastic-stack.sh
+++ capstone/install-elastic-stack.sh
@@ -12,20 +12,21 @@ apt-get -y update
 # Install Elasticsearch.
 apt-get -y install elasticsearch=7.8.1
 
+# Kropka jest znakiem specjalnym.
 # Bind Elastic to IP.
 sed "s/#network.host:.*/network.host: \"elastic-stack-instance\"/" /etc/elasticsearch/elasticsearch.yml
 
 # Create or uncomment node.
-if [ grep -Fxq "#node.name:" /etc/elasticsearch/elasticsearch.yml -eq 0 ]; then
-    echo "node.name: \"Capstone\"" >> /etc/elasticsearch/elasticsearch.yml
-else
+if [ grep -xq "#node.name:" /etc/elasticsearch/elasticsearch.yml -eq 0 ]; then
     sed -e "s/#node.name:.*/node.name: \"Capstone\"/" /etc/elasticsearch/elasticsearch.yml
+else
+    echo "node.name: \"Capstone\"" >> /etc/elasticsearch/elasticsearch.yml
 fi
 
-if [ grep -Fxq "#cluster.initial_master_nodes" /etc/elasticsearch/elasticsearch.yml -eq 0 ]; then
-    echo "cluster.initial_master_nodes: [\"Capstone\"]" >> /etc/elasticsearch/elasticsearch.yml
-else
+if [ grep -xq "#cluster.initial_master_nodes" /etc/elasticsearch/elasticsearch.yml -eq 0 ]; then
     sed "s/#cluster.initial_master_nodes:*/cluster.initial_master_nodes: [\"Capstone\"]/" /etc/elasticsearch/elasticsearch.yml
+else
+    echo "cluster.initial_master_nodes: [\"Capstone\"]" >> /etc/elasticsearch/elasticsearch.yml
 fi
 
 # Start Elasticsearch service
