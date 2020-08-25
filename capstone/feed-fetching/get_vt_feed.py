077b179043a4c0705fee4753a3a5c1abd172b784
Egor Karpov <kkarpea@gmail.com>
2020-08-25 10:36:18 +0300

    VT feed fetching (#35)

---
+++ capstone/feed-fetching/get_vt_feed.py
@@ -0,0 +1,57 @@
+import bz2
+import io
+import itertools
+import json
+import os
+import requests
+from time import time
+from datetime import datetime, timedelta
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch('elastic-stack-instance:9200')
+API_KEY = os.environ.get('VTKEY')  # is there a better way to store api key?
+ALLOWED_FIELDS = {'capabilities_tags', 'first_submission_date', 'last_submission_date', 'last_analysis_stats',
+                  'meaningful_name', 'sigma_analysis_stats', 'type_description', 'type_tag', 'reputation', 'vhash',
+                  'sha1', 'sha256', 'imphash', 'md5', 'times_submitted', 'authentihash', 'tags', 'timestamp'}
+
+
+def get_feed(timedelta_mins: int):
+    filename = (datetime.utcnow() - timedelta(minutes=timedelta_mins)).strftime('%Y%m%d%H%M')
+    response = requests.get('https://www.virustotal.com/api/v3/feeds/files/{}'.format(filename),
+                            headers={'X-Apikey': API_KEY})
+
+    if response.status_code != 200:
+        raise Exception('Feed fetching for {} failed with {}: {}'.format(filename, response.status_code, response.text))
+    stream = io.BytesIO(response.content)
+
+    for line in bz2.open(stream):
+        yield json.loads(line.decode())['attributes']
+
+
+def prepare_doc(doc: dict):
+    """
+    Prepare file from VirusTotal feed to adding to Elasticsearch.
+
+    Args:
+      - doc: document from VT Feed.
+    Returns:
+      the doc left with only allowed fields and formatted for elastic timestamps.
+    """
+    summary_doc = {key: val for key, val in doc.items() if key in ALLOWED_FIELDS}
+
+    if 'first_submission_date' in summary_doc:
+        summary_doc['first_submission_date'] = datetime.fromtimestamp(summary_doc['first_submission_date'])
+    if 'last_submission_date' in summary_doc:
+        summary_doc['last_submission_date'] = datetime.fromtimestamp(summary_doc['last_submission_date'])
+    return summary_doc
+
+
+def main():
+    for doc in itertools.chain.from_iterable(get_feed(i) for i in range(60, 75)):
+        prepared = prepare_doc(doc)
+        date = datetime.now().strftime("%Y.%m.%d")
+        es.index(index='virustotal-feed-{}'.format(date), body=prepared)
+
+
+if __name__ == "__main__":
+    main()
---
+++ capstone/feed-fetching/test_get_vt_feed.py
@@ -0,0 +1,68 @@
+import bz2
+import io
+import os
+import pytest
+import requests
+import unittest
+from datetime import datetime
+from unittest.mock import patch, Mock
+
+import get_vt_feed
+
+
+class MockResponse:
+    def __init__(self, status_code=200, text='', content=b''):
+        self.status_code = status_code
+        self.text = text
+        self.content = content
+
+
+
+class TestFeedFetching(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.vtkey = get_vt_feed.API_KEY
+        get_vt_feed.API_KEY = 'test-key'
+
+    @classmethod
+    def tearDownClass(cls):
+        get_vt_feed.API_KEY = cls.vtkey
+
+    @unittest.mock.patch('requests.get', return_value=MockResponse(status_code=200, content=b'vt-feed'))
+    @unittest.mock.patch('bz2.open', return_value=io.BytesIO(b'{ "attributes": { "attr1": "val1", "attr2": "val2" } }'))
+    def test_get_feed(self, bz2_open, requests_get):
+        result = list(get_vt_feed.get_feed(0))
+
+        filename = datetime.utcnow().strftime('%Y%m%d%H%M')
+        requests.get.assert_called_once_with('https://www.virustotal.com/api/v3/feeds/files/{}'.format(filename),
+                                            headers={'X-Apikey': 'test-key'})
+        bz2.open.assert_called_once()
+        assert result == [{'attr1': 'val1', 'attr2': 'val2'}]
+    
+
+    @unittest.mock.patch('requests.get', return_value=MockResponse(status_code=400, text='bad test request'))
+    @unittest.mock.patch('bz2.open', return_value=io.BytesIO(b'{ "attributes": { "attr1": "val1", "attr2": "val2" } }'))
+    def test_get_feed_fail(self, bz2_open, requests_get):
+        filename = datetime.utcnow().strftime('%Y%m%d%H%M')
+        with self.assertRaisesRegex(Exception, 'Feed fetching for {} failed with 400: bad test request'.format(filename)):
+            list(get_vt_feed.get_feed(0))
+
+
+    def test_prepare_doc(self):
+        assert get_vt_feed.prepare_doc({'md5': '', 'md6': '', 'sha1': '', 'sha256': '', 'field': ''}) == \
+            {'md5': '', 'sha1': '', 'sha256': ''}
+        
+        assert get_vt_feed.prepare_doc({'field1': 1, 'field2': 2, 'first_submission_date': 1598269912}) == \
+            {'first_submission_date': datetime(2020, 8, 24, 14, 51, 52)}
+        
+        assert get_vt_feed.prepare_doc({'field1': 1, 'field2': 2, 'last_submission_date': 1598269912}) == \
+            {'last_submission_date': datetime(2020, 8, 24, 14, 51, 52)}
+
+        feed = {field: field for field in get_vt_feed.ALLOWED_FIELDS}
+        feed.update({'first_submission_date': 0, 'last_submission_date': 0})
+        result = get_vt_feed.prepare_doc(feed)
+        feed.update({'first_submission_date': datetime(1970, 1, 1, 3, 0), 'last_submission_date': datetime(1970, 1, 1, 3, 0)})
+        assert result == feed
+
+if __name__ == "__main__":
+    unittest.main()
