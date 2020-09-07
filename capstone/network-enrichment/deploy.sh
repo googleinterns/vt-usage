7c0c2305c29ad22bbe76fee8f8a6ca72b23c4785
Egor Karpov <kkarpea@gmail.com>
2020-09-07 12:45:33 +0300

    deployment script

--- capstone/network-enrichment/deploy.sh
+++ capstone/network-enrichment/deploy.sh
@@ -7,16 +7,28 @@ ssh wazuh-manager << EOF
     chmod 750 /var/ossec/integrations/custom-vt-network
     chown root:ossec /var/ossec/integrations/custom-vt-network
 
-    # TODO : ADD INTEGRATION TO OSSEC.CONF
+    OSSECCONF=$(cat /var/ossec/etc/ossec.conf)
+    if [[ '$OSSECCONF' != *'custom-vt-network'* ]]; then
+        echo "
+<ossec_config>
+    <integration>
+        <name>custom-vt-network</name>
+        <group>sshd</group>
+        <api_key>bd23bf03d9975b3b5c22b5963c0b63293fa38170daaf2d8a5ab3c84747819799</api_key>
+        <alert_format>json</alert_format>
+    </integration>
+</ossec_config>" >> /var/ossec/etc/ossec.conf
+    fi
 
     if [ ! -f /var/ossec/ruleset/rules/1000-virustotal-enrichment_rules.xml ]; then
-        echo "<group name="virustotal-enrichment,">
-                <rule id="100010" level="5">
-                    <decoded_as>json</decoded_as>
-                    <field name="integration">custom-vt-network</field>
-                    <description>Virustotal data enrichment.</description>
-                </rule>
-            </group>" > /var/ossec/ruleset/rules/1000-virustotal-enrichment_rules.xml
+        echo "
+<group name="virustotal-enrichment,">
+    <rule id="100010" level="5">
+        <decoded_as>json</decoded_as>
+        <field name="integration">custom-vt-network</field>
+        <description>Virustotal data enrichment.</description>
+    </rule>
+</group>" > /var/ossec/ruleset/rules/1000-virustotal-enrichment_rules.xml
     fi
 
     systemctl restart wazuh-manager
