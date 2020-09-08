5885eeaaa665877d06b0bf64c79dc65477d5dbf8
Egor Karpov <kkarpea@gmail.com>
2020-09-08 13:10:29 +0300

    added f-strings, removed unused import

--- capstone/network-enrichment/custom_vt_network.py
+++ capstone/network-enrichment/custom_vt_network.py
@@ -5,7 +5,6 @@ import logging
 import os
 import requests
 import sys
-import time
 from collections import defaultdict
 from socket import socket, AF_UNIX, SOCK_DGRAM
 
@@ -15,14 +14,13 @@ PWD = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 def send_event(msg, agent = None):
     if not agent or agent["id"] == "000":
-        string = '1:custom-vt-network:{0}'.format(json.dumps(msg))
+        string = f'1:custom-vt-network:{json.dumps(msg)}'
     else:
-        string = '1:[{0}] ({1}) {2}->custom-vt-network:{3}'.format(agent["id"], agent["name"], agent["ip"] if "ip" in agent else "any", json.dumps(msg))
+        string = f'1:[{agent["id"]}] ({agent["name"]}) {agent["ip"] if "ip" in agent else "any"}->custom-vt-network:{json.dumps(msg)}'
 
     logging.info(string)
 
-    pwd = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    socket_addr = f'{pwd}/queue/ossec/queue'
+    socket_addr = f'{PWD}/queue/ossec/queue'
 
     sock = socket(AF_UNIX, SOCK_DGRAM)
     sock.connect(socket_addr)
@@ -33,7 +31,7 @@ def check_existing_ip(ip):
     req = {'query': {'bool': {'must': [{'match': {'data.integration': 'custom-vt-network'}}, {'match': {'data.srcip': ip}}]}}}
     resp = requests.get('http://elastic-stack-instance:9200/wazuh-alerts-3.x-*/_search', json=req)
     if resp.status_code != 200:
-        logging.error('Request to elasticsearch failed')
+        logging.error(f'Request to elasticsearch failed: {resp.text}')
         raise Exception('Request to elasticsearch failed')
     return resp.json()['hits']['total']['value'] != 0
 
@@ -59,7 +57,7 @@ def request_virustotal_info(alert, api_key):
     alert_output['vt-enrichment'] = dict()
 
     srcip = alert['data']['srcip']
-    vt_data_ip = requests.get('https://www.virustotal.com/api/v3/ip_addresses/{}'.format(srcip), headers={'x-apikey': api_key})
+    vt_data_ip = requests.get(f'https://www.virustotal.com/api/v3/ip_addresses/{srcip}', headers={'x-apikey': api_key})
 
     alert_output['vt-enrichment']['error'] = vt_data_ip.status_code
     alert_output['vt-enrichment']['description'] = errors_desc[vt_data_ip.status_code]
@@ -68,7 +66,7 @@ def request_virustotal_info(alert, api_key):
         vt_data_ip_filtered = {key: val for key, val in vt_data_ip.json()['data']['attributes'].items() if key in allowed_fields_ip}
         alert_output['vt-enrichment'].update(vt_data_ip_filtered)
 
-    vt_data_urls = requests.get('https://www.virustotal.com/api/v3/ip_addresses/{}/urls'.format(srcip), headers={'x-apikey': api_key})
+    vt_data_urls = requests.get(f'https://www.virustotal.com/api/v3/ip_addresses/{srcip}/urls', headers={'x-apikey': api_key})
 
     if vt_data_urls.status_code == 200:
         related_urls = [url['url'] for url in vt_data_urls.json()['data']]
@@ -88,11 +86,11 @@ def main(alert_filename, api_key, hook_url):
         alert_output = request_virustotal_info(alert_json, api_key)
         send_event(alert_output, alert_json['agent'])
     else:
-        logging.info('IP found again: ' + alert_json['data']['srcip'])
+        logging.info(f'IP found again: {alert_json["data"]["srcip"]}')
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='{0}/logs/integrations.log'.format(pwd),
+    logging.basicConfig(filename=f'{PWD}/logs/integrations.log',
                         filemode='a',
                         format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                         datefmt='%H:%M:%S',
